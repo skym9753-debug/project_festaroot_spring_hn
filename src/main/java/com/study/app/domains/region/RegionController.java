b0df2a1ae7a5b1e0590ee6b4806b1a8e1592cc83
하늘 <skym9753@gamil.com>
2026-06-04 09:43:38 +0900

    regionController 클래스 추가함ㅎ

---
+++ src/main/java/com/study/app/domains/region/RegionController.java
@@ -0,0 +1,31 @@
+package com.study.app.domains.region;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/region")
+public class RegionController {
+
+   @Autowired
+   private RegionMasterService regionMasterService;
+
+   public RegionController(RegionMasterService regionMasterService) {
+      this.regionMasterService = regionMasterService;
+   }
+
+   @GetMapping("/sido")
+   public List<RegionMasterDTO> getSidoList() {
+      return regionMasterService.getSidoList();
+   }
+
+   @GetMapping("/sigungu")
+   public List<RegionMasterDTO> getSigunguList(@RequestParam String regionCode) {
+      return regionMasterService.getSigunguList(regionCode);
+   }
+}
